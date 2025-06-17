cfc6de55d70b28a27f881eecf092e9e518a9eaef
preethilatadanguria <preethilata9999@gmail.com>
2025-06-17 13:26:35 +0530

    Update MongoDB connection string to use env variable

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 
 // MongoDB Connect
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
